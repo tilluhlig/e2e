4eca39712f08b9d0a94716398c7e60a8670fc6ef
Till Uhlig <till.uhlig@gmx.net>
2020-10-27 18:35:58 +0100

    unsigned postfix added

--- tests/testUtils.h
+++ tests/testUtils.h
@@ -8,7 +8,7 @@ uint8_t *outTestDataA() {
 }
 
 uint8_t *inTestDataA(uint8_t crc, uint8_t counter) {
-	uint8_t temp[8] = { crc,counter & 0x0F,0x00,0x00,0x00,0x00,0x00,0x00 };
+	uint8_t temp[8] = { crc,counter & 0x0Fu,0x00,0x00,0x00,0x00,0x00,0x00 };
 	memcpy(testdata, temp, sizeof(testdata));
 	return testdata;
 }
@@ -20,7 +20,7 @@ uint8_t *outTestDataB() {
 }
 
 uint8_t *inTestDataB(uint8_t crc, uint8_t counter) {
-	uint8_t temp[8] = { crc,counter & 0x0F,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF };
+	uint8_t temp[8] = { crc,counter & 0x0Fu,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF };
 	memcpy(testdata, temp, sizeof(testdata));
 	return testdata;
 }
@@ -32,7 +32,7 @@ uint8_t *outTestDataC() {
 }
 
 uint8_t *inTestDataC(uint8_t crc, uint8_t counter) {
-	uint8_t temp[8] = { crc,counter & 0x0F,0x02,0x03,0x04,0x05,0x06,0x07 };
+	uint8_t temp[8] = { crc,counter & 0x0Fu,0x02,0x03,0x04,0x05,0x06,0x07 };
 	memcpy(testdata, temp, sizeof(testdata));
 	return testdata;
 }
