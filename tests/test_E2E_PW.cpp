db1fe08c1e38c8b7b2efe599d25d3e9fe6e72b80
Till Uhlig <till.uhlig@gmx.net>
2020-10-30 18:21:47 +0100

    data length increased to 64

--- tests/test_E2E_PW.cpp
+++ tests/test_E2E_PW.cpp
@@ -18,7 +18,7 @@ TEST(E2E_PW, E2EPW_Init) {
 	e2e_checkState var1_checkState;
 	e2e_sm_state var1_smState;
 	e2e_error_state ret;
-	E2E_P11Config(&config, 8, E2E_DATAID_BOTH, 0, 8, 0, 0x129, 2);
+	E2E_P11Config(&config, 64, E2E_DATAID_BOTH, 0, 8, 0, 0x129, 2);
 	E2E_SMConfig(&config, 5, 3, 3, 3, 1, 1, 1);
 	ret = E2EPW_Init(&config, &var1_checkState, &var1_protectState, &var1_smState, &var1_write, &var1_read);
 	EXPECT_EQ(ret, E2E_E_OK);
